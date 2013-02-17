722eb3067ecc9f036f2c09147f0d1c90eaf9fe10
Eric Peyton <ea.peyton@gmail.com>
2013-02-16 19:55:27 -0500

    Added all songs. Added output for parser testing.

--- dylan.py
+++ dylan.py
@@ -1,4 +1,5 @@
 # Dylan
+import os, os.path
 
 class LyricParser():
     def __init__(self):
@@ -9,25 +10,39 @@ class LyricParser():
         htmlLyrics = self.findHtmlLyics(html)
         htmlLyrics = self.removeStartingSpace(htmlLyrics)
         lyrics = self.removeHtmlTags(htmlLyrics)
+        lyrics = self.removePunctuation(lyrics)
         return lyrics
-        
     
     def findHtmlLyics(self,html):
-        start = rainyDayHTML.find(self.START_MARK)
-        end = rainyDayHTML.find(self.END_MARK)
+        start = html.find(self.START_MARK)
+        end = html.find(self.END_MARK)
         return html[start+len(self.START_MARK):end]
     
     def removeStartingSpace(self, html):
-        html = html.replace('\t','')
+        while(html[0] in [' ','\t','\n']):
+            html = html[1:]
         return html
     
     def removeHtmlTags(self, html):
-        html = html.replace('<p>','')
-        html = html.replace('</p>','')
-        html = html.replace('<br>','')
+        for tag in ['<p>','</p>','<br>','<br />']:
+            html = html.replace(tag,'')
         return html
 
-rainyDayHTML = open('songs/RainyDay.htm','r').read()
+    def removePunctuation(self, lyrics):
+        for punct in ['\x92','\x93','\x94',',','?','"']:
+            lyrics = lyrics.replace(punct,'')
+        return lyrics
+
+output = open('output.txt','w')
+
 parser = LyricParser()
-rainyDay = parser.parse(rainyDayHTML)
-print rainyDay
+for root, _, files in os.walk('songs'):
+    for file in files:
+        print file
+        output.write(file)
+        filePath = os.path.join(root,file)
+        fileHTML = open(filePath,'r').read().decode('utf-8')
+        lyrics = parser.parse(fileHTML)
+        output.write(lyrics)
+
+output.close()
